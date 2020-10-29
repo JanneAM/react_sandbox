96213a1f05396f82200b013fb60a1e4ccdf14cf7
JanneAM <janne.majasalmi@gmail.com>
2020-10-29 13:53:24 +0200

    added state for counter

--- task3_counter/src/App.js
+++ task3_counter/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    likes:0
+  };
+
   render() {
     return (
       <div>
-        <h1>Total likes:</h1>
+        <h1>Total likes: {this.state.likes}</h1>
       </div>
     );
   }
